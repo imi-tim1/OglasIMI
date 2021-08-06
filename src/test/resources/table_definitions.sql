fe2da83698e39e2c69cac7aceff3ff2d90942397
Sonja Galovic <basketballplayerkg@gmail.com>
2021-08-06 21:05:55 +0200

    dodate tabele

--- src/test/resources/table_definitions.sql
+++ src/test/resources/table_definitions.sql
@@ -1,3 +1,5 @@
+use oglasimi_db;
+
 create table role
 (
     id int auto_increment,
@@ -9,9 +11,9 @@ create table role
 create table user
 (
     id int auto_increment,
-    email varchar(300) not null ,
-    role_id int not null ,
-    approved bool not null ,
+    email varchar(300) not null,
+    role_id int not null,
+    approved bool not null,
     constraint primary key (id),
     constraint unique key (email),
     constraint foreign key (role_id) references role (id)
