5fd51be84027da3b04e19d4f0696b67d89253790
Srdjan Todorovic <tsrdjan@pm.me>
2021-08-06 16:47:08 +0200

    Initial commit

---
+++ src/main/java/com/tim1/oglasimi/OglasimiApplication.java
@@ -0,0 +1,13 @@
+package com.tim1.oglasimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OglasimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OglasimiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim1/oglasimi/OglasimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim1.oglasimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OglasimiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
