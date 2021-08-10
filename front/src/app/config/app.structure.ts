14a7811b104b268558794a13bd4216b654adf0c9
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-10 22:45:33 +0200

    Definisana struktura fronta

---
+++ front/src/app/config/app.structure.ts
@@ -0,0 +1,21 @@
+const app_structure = 
+{
+    pages: "PAGE folders",
+    
+    components: "COMPONENT folders",
+    
+    api: 
+    {
+        services: "SERVICE folders",
+        interfaces: "TS files"
+    },
+
+    util: 
+    {
+        services: "SERVICE folders",
+        interfaces: "TS files"
+    },
+
+    config: "TS files"
+}
+
