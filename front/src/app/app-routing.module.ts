0a63581430e81e830012fd40488e5841c23b3374
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-11 17:29:29 +0200

    .

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './_components/_pages/home-page/home-page.component';
 import { LoginPageComponent } from './_components/_pages/login-page/login-page.component';
 import { RegisterPageComponent } from './_components/_pages/register-page/register-page.component';
 
 const routes: Routes = [
-  { path: 'login', component: RegisterPageComponent },
-  { path: 'register', component: LoginPageComponent },
-  { path: '', component: AppComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
