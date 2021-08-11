7474a8de7ab59bbf1768b7455afc264998291cb0
Srdjan Todorovic <tsrdjan@pm.me>
2021-08-11 17:59:26 +0200

    api/login: izmenjeno ponasanje u jednoj od situacija; prepravljeni test primeri

--- src/main/java/com/tim1/oglasimi/controller/LoginController.java
+++ src/main/java/com/tim1/oglasimi/controller/LoginController.java
@@ -7,13 +7,17 @@ import com.tim1.oglasimi.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/login")
+@Validated
 public class LoginController {
 
     private final LoginService loginService;
@@ -29,7 +33,7 @@ public class LoginController {
      * Postoji nekoliko mogucih dogadjaja prilikom poziva ovog metoda:
      * <ol>
      * <li>korisnik je vec autentifikovan i pokusava da pristupi endpoint-u: zahtev se odbija uz HTTP status kod 403
-     * (Forbidden) i neinicijalizovan objekat tipa {@link Model}</li>
+     * (Forbidden) i objekat tipa {@link Model} u kome se nalazi prosledjeni JWT korisnika</li>
      * <li>korisnik nije autentifikovan
      * <ol>
      * <li>uneti podaci nisu ispravni: vraca se HTTP status kod 401 (Unauthorized) i neinicijalizovan objekat
@@ -37,25 +41,27 @@ public class LoginController {
      * <li>uneti podaci su ispravni, ali korisnik jos uvek nije potvrdjen od strane administratora: vraca se HTTP
      * status kod 403 (Forbidden)  i neinicijalizovan objekat tipa {@link Model} </li>
      * <li>uneti podaci su ispravni i korisnik je potvrdjen od strane administratora: vraca se HTTP status kod 200
-     * (OK), dodeljuje mu se JWT koji se nalazi u objetku tipa {@link Model}</li>
+     * (OK), dodeljuje mu se JWT koji se nalazi u objetku tipa {@link Model} koji se vraca</li>
      * </ol></li>
      * </ol>
      *
      * @param loginCredentials objekat u kome se prihvataju jwt, email i lozinka koji su poslati od strane klijenta
      * @return vraca se {@link org.springframework.http.ResponseEntity} sa objekatom tipa Model u kome se
-     * nalazi JWT i odgovarajuci HTTP status
+     * moze naci JWT i odgovarajuci HTTP status
      * @see LoginCredentials
      * @see LoginResponse
      * @see ResponseEntity
      */
     @PostMapping
-    public ResponseEntity<?> login(@RequestBody LoginCredentials loginCredentials) {
+    public ResponseEntity<Model> login( @Valid @RequestBody LoginCredentials loginCredentials) {
 
         String jwt = loginCredentials.getJwt();
         Model returnModel = new Model();
 
         // check if authenticated user send the request
         if(  jwt != null && jwt != "" ) {
+            returnModel.setJwt( loginCredentials.getJwt() );
+
             return ResponseEntity
                     .status(HttpStatus.FORBIDDEN)
                     .body( returnModel );
