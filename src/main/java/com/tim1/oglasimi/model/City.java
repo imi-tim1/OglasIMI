f069c798d2afc8ac27bd9ebf5ab877f9b73fbddd
Mihajlo Jankovic <mihajlojankovic44@gmail.com>
2021-08-13 17:59:29 +0200

    Dopuna validacija u modelima vezanim za Job

--- src/main/java/com/tim1/oglasimi/model/City.java
+++ src/main/java/com/tim1/oglasimi/model/City.java
@@ -1,8 +1,15 @@
 package com.tim1.oglasimi.model;
 
+import javax.validation.constraints.*;
+
 public class City
 {
+    @Min(1)
+    @Max(Integer.MAX_VALUE)
     private int id;
+
+    @NotBlank @NotNull
+    @Size( min = 2, max = 30, message = "The length of name must be between 2 and 30 characters" )
     private String name;
 
     public int getId()
