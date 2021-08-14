b88b29d6ed0c172af4cca5ed1a6d93eea3e91f69
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-14 20:48:48 +0200

    Front: Logout strana, Dugme za logout i rutiranje u vezi toga

---
+++ front/src/app/_components/_pages/logout-page/logout-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RedirectRoute } from 'src/app/_utilities/_constants/routing.properties';
+import { JWTUtil } from 'src/app/_utilities/_helpers/jwt-util';
+
+@Component({
+  selector: 'app-logout-page',
+  templateUrl: './logout-page.component.html',
+})
+export class LogoutPageComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    JWTUtil.delete();
+    this.router.navigate([RedirectRoute.DEFAULT]);
+  }
+
+}
