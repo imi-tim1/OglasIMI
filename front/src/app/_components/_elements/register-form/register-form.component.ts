06ad420037daa37f6c29dce5636d1480642f85d5
Sonja Galovic <basketballplayerkg@gmail.com>
2021-08-14 18:03:23 +0200

    Front: register page v1

--- front/src/app/_components/_elements/register-form/register-form.component.ts
+++ front/src/app/_components/_elements/register-form/register-form.component.ts
@@ -6,8 +6,14 @@ import { EmployerService } from 'src/app/_utilities/_middleware/_services/employ
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+
+
+  showApplicantFormBool: boolean = true;
+  showEmployerFormBool: boolean = false;
+
 
   constructor(public applicantService: ApplicantService,
               public employerService: EmployerService) { }
@@ -15,4 +21,33 @@ export class RegisterFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showMeApplicantForm() {
+    if (this.showApplicantFormBool == false) //trenutno nije prikazana applicant forma
+    {
+      this.showEmployerFormBool = false; //sakrij employer
+      this.showApplicantFormBool = true; //prikazi applicant
+      
+      let a = <HTMLSelectElement>document.getElementById('appBtn');
+      a.style.backgroundColor = "blue";
+      a.style.color = "white";
+      let e = <HTMLSelectElement>document.getElementById('empBtn');
+      e.style.backgroundColor = "white";
+      e.style.color = "blue;"
+    }
+  }
+
+  showMeEmployerForm() {
+    if (this.showEmployerFormBool == false)
+    {
+      this.showApplicantFormBool = false;
+      this.showEmployerFormBool = true;
+
+      let a = <HTMLSelectElement>document.getElementById('appBtn');
+      a.style.backgroundColor = "white";
+      a.style.color = "blue;"
+      let e = <HTMLSelectElement>document.getElementById('empBtn');
+      e.style.backgroundColor = "blue";
+      e.style.color = "white;"
+    }
+  }
 }
