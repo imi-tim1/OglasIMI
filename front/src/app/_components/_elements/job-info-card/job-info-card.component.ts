8a5c7308cc5f639016657e845d20bd945ce79ec7
Sonja Galovic <basketballplayerkg@gmail.com>
2021-08-17 13:32:45 +0200

    Front: job-info-page / v3

--- front/src/app/_components/_elements/job-info-card/job-info-card.component.ts
+++ front/src/app/_components/_elements/job-info-card/job-info-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { UserRole } from 'src/app/_utilities/_api/_data-types/enums';
+import { JWTUtil } from 'src/app/_utilities/_helpers/jwt-util';
 import { JobService } from 'src/app/_utilities/_middleware/_services/job.service';
+import { EmployerService } from 'src/app/_utilities/_middleware/_services/employer.service';
 
 @Component({
   selector: 'app-job-info-card',
@@ -9,16 +12,31 @@ export class JobInfoCardComponent implements OnInit {
 
   @Input() id: number = 0;
 
-  constructor(public jobService: JobService) { }
+  constructor(public jobService: JobService,
+              public employerService: EmployerService) { }
 
   ngOnInit(): void {
     console.log("id: " + this.id);
 
     this.jobService.getJob(this.id);
+
+    console.log("drugi put id: " + this.id);
+    //console.log("NESTO PRE EMPLOYER ID-A"); 
+    //console.log("EMPLOYER ID: " + this.jobService.job.employer.id);
+    //this.employerService.getEmployer(this.jobService.job.employer.id);
   }
 
   stampaj() {
     console.log(" STAMPAJ  id: " + this.id);
+  }
+
+  isApplicant() {
+    return JWTUtil.getRole() as UserRole == UserRole.Applicant;
+  }
+
+  applyMe() {
+    console.log(this.employerService.employer?.email);
+    this.jobService.applyToJob(this.id);
   }
 
 }
