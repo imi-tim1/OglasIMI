50a122f7a06850362357e6be6c6e67d791b7d1ca
Srdjan Todorovic <tsrdjan@pm.me>
2021-08-17 15:11:50 +0200

    prebaceni property-ji email i hashedPassword iz EndUser u User

--- src/main/java/com/tim1/oglasimi/model/EndUser.java
+++ src/main/java/com/tim1/oglasimi/model/EndUser.java
@@ -1,23 +1,9 @@
 package com.tim1.oglasimi.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class EndUser extends User {
-    @NotBlank
-    @NotNull
-    @Email( message = "Email address is not valid" )
-    private String email;
-
-    @NotNull
-    @NotBlank
-    @Size( max = 300, message = "Hashed password length exceeded maximum allowed length" )
-    private String hashedPassword;
 
     @Size( min = 5000, max = 65000,
             message = "The length for base64 encoded picture must be between 5000 and 65000 characters" )
@@ -36,30 +22,9 @@ public class EndUser extends User {
             String hashedPassword,
             String pictureBase64,
             String phoneNumber) {
-        super(id);
-        this.email = email;
-        this.hashedPassword = hashedPassword;
+        super(id,email,hashedPassword);
         this.pictureBase64 = pictureBase64;
         this.phoneNumber = phoneNumber;
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @JsonIgnore
-    public String getHashedPassword() {
-        return hashedPassword;
-    }
-
-    @JsonProperty
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
     }
 
     public String getPictureBase64() {
@@ -82,8 +47,8 @@ public class EndUser extends User {
     public String toString() {
         return "EndUser{" +
                 "id=" + super.getId() +
-                ", email='" + email + '\'' +
-                ", hashedPassword='" + hashedPassword + '\'' +
+                ", email='" + super.getEmail() + '\'' +
+                ", hashedPassword='" + super.getHashedPassword() + '\'' +
                 ", pictureBase64='" + pictureBase64 + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
--- src/main/java/com/tim1/oglasimi/model/User.java
+++ src/main/java/com/tim1/oglasimi/model/User.java
@@ -3,8 +3,7 @@ package com.tim1.oglasimi.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.*;
 
 public class User {
 
@@ -12,11 +11,24 @@ public class User {
     @Max( Integer.MAX_VALUE )
     private int id;
 
+
+    @NotBlank
+    @NotNull
+    @Email( message = "Email address is not valid" )
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @Size( max = 300, message = "Hashed password length exceeded maximum allowed length" )
+    private String hashedPassword;
+
     public User() {
     }
 
-    public User(int id) {
+    public User(int id, String email, String hashedPassword) {
         this.id = id;
+        this.email = email;
+        this.hashedPassword = hashedPassword;
     }
 
     @JsonProperty
@@ -29,10 +41,30 @@ public class User {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    @JsonProperty
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
+                ", email='" + email + '\'' +
+                ", hashedPassword='" + hashedPassword + '\'' +
                 '}';
     }
 }
