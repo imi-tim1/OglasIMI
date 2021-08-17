c762e1aa16a8289da0c4eaea770fcb8c1d2f16be
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-17 13:30:03 +0200

    Front: Admins Dashboard v0.2

---
+++ front/src/app/_components/_elements/applicant-info-card/applicant-info-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-info-card',
+  templateUrl: './applicant-info-card.component.html',
+})
+export class ApplicantInfoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
