a938a47d04a0cec7139a996101b3a2af58a9dbdd
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-26 19:21:24 +0200

    [Front] Dodate komponente (prazne): job-comment-card, job-comments-list

---
+++ front/src/app/_components/_elements/job-comment-card/job-comment-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-comment-card',
+  templateUrl: './job-comment-card.component.html',
+})
+export class JobCommentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front/src/app/_components/_elements/job-comments-list/job-comments-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-comments-list',
+  templateUrl: './job-comments-list.component.html',
+})
+export class JobCommentsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
