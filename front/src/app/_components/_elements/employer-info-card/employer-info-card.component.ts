264dd639f59abd45423b3e5c7a63ec2f6774172b
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-28 00:32:49 +0200

    [Front] Ocenjivanje poslodavca v0.2 (Potpuno radi 'fetch')

--- front/src/app/_components/_elements/employer-info-card/employer-info-card.component.ts
+++ front/src/app/_components/_elements/employer-info-card/employer-info-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons';
-import { Employer } from 'src/app/_utilities/_api/_data-types/interfaces';
+import { Employer, Job, RatingResponse } from 'src/app/_utilities/_api/_data-types/interfaces';
 import { DEFAULT_PROFILE_PICTURE } from 'src/app/_utilities/_constants/raw-data';
 import { JWTUtil } from 'src/app/_utilities/_helpers/jwt-util';
+import { EmployerService } from 'src/app/_utilities/_middleware/_services/employer.service';
+import { ApplicantService } from 'src/app/_utilities/_middleware/_services/applicant.service';
 
 @Component({
   selector: 'app-employer-info-card',
@@ -13,6 +15,7 @@ export class EmployerInfoCardComponent implements OnInit {
 
   @Input() public emp: Employer | null = null;
   @Input() public rating: number = -1;
+  @Input() public rateDissabled: boolean = false;
 
   public myRating: number = 0;
   public showRateOption: boolean = false;
@@ -24,20 +27,23 @@ export class EmployerInfoCardComponent implements OnInit {
   iconStar = faStar;
   iconStarEmpty = faStarReg;
 
-  constructor() { }
+  constructor(
+    private employerService: EmployerService,
+    private applicantService: ApplicantService
+  ) { }
 
   ngOnInit(): void {
     this.fetchRating();
-    console.log('>>>>>> init ' + this.emp?.id);
   }
 
   // --- Actions ---
 
-  fetchRating() {
-    // TEMP
-    // TODO: API Call ->
-    this.rating = 3.5;
-    this.allreadyRated = !true;
+  fetchRating() 
+  {
+    if(!this.emp)
+      return;
+
+    this.employerService.getEmployersRating(this.emp.id, this, this.cbSuccessGetRating);
   }
 
   toggleRateOption() {
@@ -57,12 +63,26 @@ export class EmployerInfoCardComponent implements OnInit {
   // --- Auth ---
 
   canRate() {
-    return !this.allreadyRated;
+    return !this.allreadyRated && !this.rateDissabled;
   }
 
   // --- API Callbacks ---
 
-  cbSuccessRate(self: any) {
+  cbSuccessGetRating(self: any, data: RatingResponse) 
+  {
+    self.rating = data.rating;
+    self.allreadyRated = data.alreadyRated;
+
+    self.applicantService.getApplicantsJobs(JWTUtil.getID(), self, 
+      (self: any, data: Job[]) => 
+      {
+        self.rateDissabled = !(data.find(j => j.employer.id == self.emp.id) != undefined);
+      }
+    );
+  }
+
+  cbSuccessRate(self: any) 
+  {
     self.showRateOption = false;
     // this.fetchRating(); // TODO
     this.allreadyRated = true; // TEMP
