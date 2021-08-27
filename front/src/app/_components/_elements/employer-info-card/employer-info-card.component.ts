b693e72d80985c3358cbc1d9866ab72f6f24daa0
Dragor Draganovic <dragor.draganovic@gmail.com>
2021-08-27 22:41:31 +0200

    [Front] Ocenjivanje poslodavca v0.1

--- front/src/app/_components/_elements/employer-info-card/employer-info-card.component.ts
+++ front/src/app/_components/_elements/employer-info-card/employer-info-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons';
 import { Employer } from 'src/app/_utilities/_api/_data-types/interfaces';
 import { DEFAULT_PROFILE_PICTURE } from 'src/app/_utilities/_constants/raw-data';
+import { JWTUtil } from 'src/app/_utilities/_helpers/jwt-util';
 
 @Component({
   selector: 'app-employer-info-card',
@@ -9,19 +12,60 @@ import { DEFAULT_PROFILE_PICTURE } from 'src/app/_utilities/_constants/raw-data'
 export class EmployerInfoCardComponent implements OnInit {
 
   @Input() public emp: Employer | null = null;
+  @Input() public rating: number = -1;
+
+  public myRating: number = 0;
+  public showRateOption: boolean = false;
+  allreadyRated: boolean = false;
 
   public defaultPicture: string = DEFAULT_PROFILE_PICTURE;
+
+  // Fontawesome
+  iconStar = faStar;
+  iconStarEmpty = faStarReg;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.fetchRating();
     console.log('>>>>>> init ' + this.emp?.id);
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('>>>>>> promena ' + this.emp?.id);
-    let e: Employer | null = this.emp;
-    this.emp = e;
+  // --- Actions ---
+
+  fetchRating() {
+    // TEMP
+    // TODO: API Call ->
+    this.rating = 3.5;
+    this.allreadyRated = !true;
+  }
+
+  toggleRateOption() {
+    this.showRateOption = !this.showRateOption;
+    this.myRating = 0;
+  }
+
+  clickRateStar(rate: number) {
+    this.myRating = rate;
+  }
+
+  sendMyRating() {
+    // TODO: API Call ->
+    this.cbSuccessRate(this); // TEMP
+  }
+
+  // --- Auth ---
+
+  canRate() {
+    return !this.allreadyRated;
+  }
+
+  // --- API Callbacks ---
+
+  cbSuccessRate(self: any) {
+    self.showRateOption = false;
+    // this.fetchRating(); // TODO
+    this.allreadyRated = true; // TEMP
   }
 
 }
